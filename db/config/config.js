fed9d7bb3b54b5ec50836be41f21d8acac6e855e
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-07-30 15:07:13 +0800

    update

--- db/config/config.js
+++ db/config/config.js
@@ -1,9 +1,10 @@
-require('dotenv').config({});
+require("dotenv").config({});
 module.exports = {
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	host: process.env.DB_HOST,
-	dialect: 'mysql',
-	logging: console.log
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "mysql",
+  logging: console.log,
 };
